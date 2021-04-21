1f93ec2afbefe38408013fcd3f1ac88ff01abb8d
sonoMara <73066516+sonoMara@users.noreply.github.com>
2021-04-21 13:37:33 +0200

    Update A4988pic.h

--- A4988pic.h
+++ A4988pic.h
@@ -13,11 +13,11 @@
  *              MCLR    |4                15|RA6
  *              VSS	|5   pic16f18xx	  14|VDD
  *              RB0	|6		  13|RB7    STEP2
- *    MS1	RB1	|7		  12|RB6    DIR2
+ *    MS1 RB1	|7		  12|RB6    DIR2
  *    MS2	RB2	|8		  11|RB5    STEP1
  *    MS3	RB3	|9		  10|RB4    DIR1
- *                  |					          |
- *                  |-------------------|
+ *           		|		    |
+ *                      |-------------------|
  * 
  * Libreria scritta da Enrico Maraldi and Andrea D'agostino, a.s. 20/21
  * 
